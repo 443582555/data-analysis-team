5682bdbbec0817c5da14d9443448be62061a757f
felicia <felicia@ResNet-55-17.resnet.ucsb.edu>
2018-06-03 17:41:41 -0700

    added jd.py

--- jd.py
+++ jd.py
@@ -5,18 +5,26 @@ print(check_output(["ls", "../data-analysis/jd"]).decode("utf8"))
 # All csv files are within jd folder 
 
 info = pd.read_csv("../data-analysis/jd/sku_info.csv")
-# print(info) 1000 rows x 5 columns id:1-1000
+# info 1000 rows x 5 columns id:0-999
 attr = pd.read_csv("../data-analysis/jd/sku_attr.csv")
-# print(attr)  6776 rows x 3 columns
+# attr  6776 rows x 3 columns
 sales = pd.read_csv("../data-analysis/jd/sku_sales.csv")
-# print(sales) 2127485 rows x 7 columns
+# sales 2127485 rows x 7 columns
 prom = pd.read_csv("../data-analysis/jd/sku_prom.csv")
-# print(prom) 862111 rows x 4 columns
+# prom 862111 rows x 4 columns
 
 #infoAttr=info.set_index('item_sku_id').join(attr.set_index('item_sku_id'))
-#print(info_attr) 6776 rows x 6 columns
+#info_attr 6776 rows x 6 columns
 #infoAttrSales=infoAttr.set_index('item_sku_id').join(sales.set_index('item_sku_id'))
 #key error?
 
 InfoAttrSales=sales.set_index('item_sku_id').join(info.set_index('item_sku_id')).join(attr.set_index('item_sku_id'))
-#print(InfoAttrSales)  14041831 rows x 12 columns
+# InfoAttrSales  #14041831 rows x 12 columns
+# type(InfoAttrSales)   pandas.core.frame.DataFrame
+
+# Create a new column called prom.promation where the value is 0
+# if prom.item_sku_id == -999 and 1 if not
+prom['promation'] = np.where(prom['item_sku_id']==-999, 0, 1)
+# prom
+prom = prom.rename(columns={'date': 'prom_date'})
+# prom
