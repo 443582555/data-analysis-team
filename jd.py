db1777c7c79cf5ad6819e0f9036ffb26258de07d
443582555 <yifu.luo@vanderbilt.edu>
2018-06-05 23:26:41 -0700

    revised

--- jd.py
+++ jd.py
@@ -1,20 +1,32 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from subprocess import check_output
-print(check_output(["ls", "../data-analysis/jd"]).decode("utf8"))
+import sys
+
+
+
+total = len(sys.argv)
+
+if total == 0:
+	folderAddress = "../data-analysis/jd"
+else:
+	folderAddress = sys.argv[1]
+
+
+print(check_output(["ls", ]).decode("utf8"))
 # All csv files are within jd folder 
 
-info = pd.read_csv("../data-analysis/jd/sku_info.csv")
+info = pd.read_csv(folderAddress + "/sku_info.csv")
 # info 1000 rows x 5 columns id:0-999
-attr = pd.read_csv("../data-analysis/jd/sku_attr.csv")
+attr = pd.read_csv(folderAddress + "/sku_attr.csv")
 # attr  6776 rows x 3 columns
-sales = pd.read_csv("../data-analysis/jd/sku_sales.csv")
+sales = pd.read_csv(folderAddress + "/sku_sales.csv")
 # sales 2127485 rows x 7 columns
-prom = pd.read_csv("../data-analysis/jd/sku_prom.csv")
+prom = pd.read_csv(folderAddress + "/sku_prom.csv")
 # prom 862111 rows x 4 columns
 
 #infoAttr=info.set_index('item_sku_id').join(attr.set_index('item_sku_id'))
-#info_attr 6776 rows x 6 columns
+#info_attr 6776 row:::s x 6 columns
 #infoAttrSales=infoAttr.set_index('item_sku_id').join(sales.set_index('item_sku_id'))
 #key error?
 
@@ -27,4 +39,6 @@ InfoAttrSales=sales.set_index('item_sku_id').join(info.set_index('item_sku_id'))
 prom['promation'] = np.where(prom['item_sku_id']==-999, 0, 1)
 # prom
 prom = prom.rename(columns={'date': 'prom_date'})
+
+print (prom)
 # prom
